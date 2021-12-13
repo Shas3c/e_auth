19639670484c9e4f50df674d4723acbb71f20bc3
Sahil Sharma <83185100+Shas3c@users.noreply.github.com>
2021-12-13 18:49:16 +0530

    Update settings.py

--- dprojx/settings.py
+++ dprojx/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+#STATICFILES_DIRS = [STATIC_DIR,]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
