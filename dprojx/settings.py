1dffa4c0c335cf2dde9b2ce0f673459837d0f399
Sahil Sharma <83185100+Shas3c@users.noreply.github.com>
2022-05-22 13:15:53 +0530

    Update settings.py

--- dprojx/settings.py
+++ dprojx/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'golugolu2112000@gmail.com'
-EMAIL_HOST_PASSWORD = 'Wiresh@rk2'
+#EMAIL_HOST_USER = 'your@gmail.com'
+#EMAIL_HOST_PASSWORD = 'your-password'
